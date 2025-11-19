a68b6a3f9b3b6d1639ce67b2ef88e7c5aba87465
Andrei Durdun <andrei-george.durdun@s.unibuc.ro>
2025-11-19 18:15:08 +0200

    corectare ex4

--- lab6/ex4.py
+++ lab6/ex4.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 n = 20
 d = 3
@@ -9,9 +10,17 @@ y = np.roll(x, d)
 X = np.fft.fft(x)
 Y = np.fft.fft(y)
 
-prod_fft = np.fft.ifft(X * Y)
+prod_fft = np.fft.ifft(np.conj(X) * Y)
 div_fft = np.fft.ifft(Y / X)
 
 print(f"Produs: {np.argmax(np.abs(prod_fft))}\nImpartire: {np.argmax(np.abs(div_fft))}")
 
-# Formula cu impartire reuseste sa recupereze deplasarea d, iar formula cu inmultire nu reuseste
+fig , axs = plt.subplots(2, 1)
+axs[0].plot(np.abs(prod_fft))
+axs[0].set_title('Produs')
+axs[1].plot(np.abs(div_fft))
+axs[1].set_title('Impartire')
+plt.savefig('lab6/ex4.pdf', format='pdf')
+plt.show()
+
+# Graficul formulei cu impartire arata un varf la pozitia d si in rest 0 (similar cu Dirac), iar formula cu produs arata un varf maxim la pozitia d si apoi mai multe valori mai mici
